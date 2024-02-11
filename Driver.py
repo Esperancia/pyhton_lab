4106fd6e9d210298a4a8a6624450eaf10cb1df21
Esperancia <champouxah@yahoo.fr>
2024-02-11 15:21:52 -0500

    Pycharm formatting

--- Driver.py
+++ Driver.py
@@ -4,22 +4,18 @@ from Tools.Term import Term
 
 
 class Driver:
-
-    t0 = Term("t0", 3.0, 'x', 2) # 3x^2
-    t1 = Term("t1", -2.0, 'y', 3) # -2y^3
-    t2 = Term("t2", 4.0, 'x', 4) # 4x^4
-    t3 = Term("t3", 8.0, ' ', 0) # 8
+    t0 = Term("t0", 3.0, 'x', 2)  # 3x^2
+    t1 = Term("t1", -2.0, 'y', 3)  # -2y^3
+    t2 = Term("t2", 4.0, 'x', 4)  # 4x^4
+    t3 = Term("t3", 8.0, ' ', 0)  # 8
     l1 = Polynomial('P1')
 
     print("Ajout de t0 dans l1:", l1.addTerm(t0))
     print("Ajout de t1 dans l1:", l1.addTerm(t1))
 
-
-    t4 = Term("t0", 7.0, 'x', 2) # 7x^2
+    t4 = Term("t0", 7.0, 'x', 2)  # 7x^2
     print("Ajout de t0 dans l1:", l1.addTerm(t3))
     print("Ajout de t1 dans l1:", l1.addTerm(t2))
-
-
 
     print("Termes dans l1:", [str(term) for term in l1.getAllTerms()])
     print("Exposants dans l1:", l1.getExponents())
